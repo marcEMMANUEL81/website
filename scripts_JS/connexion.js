1bfc137a5f10afeb789a6f75cf3f1a67251395ee
marcEMMANUEL81 <88037063+marcEMMANUEL81@users.noreply.github.com>
2022-10-30 22:10:54 +0000

    Add files via upload

---
+++ scripts_JS/connexion.js
@@ -0,0 +1,19 @@
+///const modal = document.querySelector(".modal")
+///const modal__button1 = document.getElementById("#button1")
+///const modal__button2 = document.getElementById("#button2")
+///const modal__button3 = document.getElementById("#button3")
+
+///console.log(modal__button1)
+
+///modal__button2.addEventListener("click", function(){
+   /// modal.classList.add("visible")
+///})
+
+///modal__button1.addEventListener("click", function(){
+   /// modal.classList.remove("visible")
+    ///window.open("../pageHtml/index.html");
+///})
+
+///modal__button3.addEventListener("click", function(){
+    ///window.open("../pageHtml/index.html");
+/// })
